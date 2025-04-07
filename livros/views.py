d5aef727879c220f9472e8ea6919f6f686704b4a
kauanAfonso <kauanafon3@gmail.com>
2025-04-07 11:20:19 -0300

    task 3

--- livros/views.py
+++ livros/views.py
@@ -3,7 +3,7 @@ from .models import Livro
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .serializer import LivroSerializer
+from .serializers import LivroSerializer
 # Create your views here.
 
 def Listar_Livros(request):

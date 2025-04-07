2f0b034cbdecadcf78579736f43b5c78bc462353
kauanAfonso <kauanafon3@gmail.com>
2025-04-07 09:49:43 -0300

    Task 2

--- livros/urls.py
+++ livros/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('templates/', views.read , name="read" )
+    path('templates/', views.Listar_Livros , name="Listar Livros" ),
+    path('api/livros', views.Listar_Livros_API)
 ]
--- livros/views.py
+++ livros/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
 from .models import Livro
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializer import LivroSerializer
 # Create your views here.
 
-def read(request):
+def Listar_Livros(request):
     try:
         livro = Livro.objects.all()
     except Exception as e:
         return render({f'erro': "{e}"})
     
     return render(request, "livros.html",  {'livros': livro})
+
+
+@api_view(['GET','POST'])
+def Listar_Livros_API(request):
+    if request.method == 'GET':
+        todos_livros = Livro.objects.all()
+        serializer = LivroSerializer(todos_livros, many=True)
+        return Response({"livros":serializer.data}, status=status.HTTP_200_OK)
+    
+    elif request.method == "POST":
+        try:
+            serializer = LivroSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'Livro criado:':serializer.data }, status=status.HTTP_201_CREATED)
+            return Response({'Erro:' "Erro de requisisão"}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'Erro: ': e}, status=status.HTTP_409_CONFLICT)

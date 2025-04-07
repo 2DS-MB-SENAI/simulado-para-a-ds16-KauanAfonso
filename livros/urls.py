752dd6ff1902860ed78a681dcdd5e195b4cf6277
kauanAfonso <kauanafon3@gmail.com>
2025-04-07 09:55:55 -0300

    Task 2 refatorando

--- livros/urls.py
+++ livros/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('livros/templates/', views.Listar_Livros , name="Listar Livros" ),
+    path('/livros/templates/', views.Listar_Livros , name="Listar Livros" ),
     path('/api/livros', views.Listar_Livros_API)
 ]

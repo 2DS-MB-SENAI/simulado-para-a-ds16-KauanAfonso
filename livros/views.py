8a8441a229df2c828ab07c7c77976949e31bc57f
kauanAfonso <kauanafon3@gmail.com>
2025-04-07 08:44:04 -0300

    task 01

---
+++ livros/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Livro
+# Create your views here.
+
+def read(request):
+    try:
+        livro = Livro.objects.all()
+    except Exception as e:
+        return render({f'erro': "{e}"})
+    
+    return render(request, "index.html",  {'livros': livro})

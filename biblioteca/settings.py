e6d3284d86973432469032ef11e425ff45c0bf38
kauanAfonso <kauanafon3@gmail.com>
2025-04-07 10:07:52 -0300

    Task 2 refatorando

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'biblioteca.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # Diretório de templates padrão
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- livros/urls.py
+++ livros/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/templates/', views.Listar_Livros , name="Listar Livros" ),
+    path('templates/', views.Listar_Livros , name="Listar_Livros" ),
     path('api/livros', views.Listar_Livros_API)
 ]
